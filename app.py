8ea8111443c801949afa21e2a65414e271fe16ba
Aji <ajienata97@gmail.com>
2026-05-07 16:07:12 +0700

    fix port for railway

--- app.py
+++ app.py
@@ -150,4 +150,6 @@ def export():
                     headers={"Content-Disposition": "attachment;filename=data_peserta.csv"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
