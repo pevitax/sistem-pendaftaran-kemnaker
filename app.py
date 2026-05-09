fd3a4fa61e6a2774cba9bb62c5c41649560563fd
pevitax <ajienata97@gmail.com>
2026-05-09 16:01:31 +0700

    fix database postgresql

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ import csv
 import io
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pendaftaran.db'
+import os
+database_url = os.environ.get('DATABASE_URL', 'sqlite:///pendaftaran.db')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'kemnaker-bina-intala-2026'
 
